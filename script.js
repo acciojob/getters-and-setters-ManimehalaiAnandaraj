4a6b43b38baf6650496ee694774127d90398ad90
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-18 20:30:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name;
+		this.age;
+	}
+	getname(){
+		return this.name;
+	}
+	getage(){
+		this.age=age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	constructor(name,age,){
+		super(name,age);
+	}
+	study(){
+		console.log(`${this.getname()}`)
+	}
+		
+	}
 
-class Teacher extends Person {}
+
+class Teacher extends Person {
+	constructor(name,age){
+		super(name,age);
+		
+	}
+	teach(){
+		console.log(`$this.getname()} is teaching`)
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
