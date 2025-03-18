57ac16c6232a98465f12b9e26e7f89d94a684125
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-18 21:29:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //complete this code
 class Person {
 	constructor(name,age){
-		this.name=name;
-		this.age=age;
+		this.name = name;
+		this.age = age;
 	}
 	get name(){
 		return this.name;
 	}
-	get age(){
-		return this.age;
+	set age(age){
+		this.age=age;
 	}
 	
 }
